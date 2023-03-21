2bd8bacd390af71abfe92b6e3792c57046f9abce
Josh Austin <127902630+JoshAustin123@users.noreply.github.com>
2023-03-21 22:10:26 +0000

    Added Restart Option

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+# Define a function to ask the player if they want to restart the quiz
+def ask_restart():
+    restart = input("Do you want to play again? (yes/no) ")
+    if restart.lower() == "yes":
+        return True
+    elif restart.lower() == "no":
+        return False
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
+        return ask_restart()
+
 # Welcome the player to the quiz and ask for their name
 print("Welcome to the Rangiora High School Quiz!\n")
 player_name = input("What is your name? ")
 
 # Create a list of questions and answers
 questions = [
-    "How many houses are in Rangiora High School? ",
+    "How many houses are there in Rangiora High School? ",
     "What day is shorter through the week? ",
-    "What house color is Hillary? ",
-    "What house color is Rutherford? ",
+    "What house colour is Hillary? ",
+    "What house colour is Rutherford? ",
     "How many terms are there? ",
     "What game do they play normally during summerfest? ",
     "What classes are in H block? ",
@@ -38,4 +49,31 @@ for i in range(len(questions)):
 print("Well done, {}! You scored {}/{}.".format(player_name, score, len(questions)))
 print("Thanks for playing the Rangiora High School Quiz!\n")
 
-#
+# Ask the player if they want to restart the quiz
+restart = ask_restart()
+
+# If the player wants to restart, run the quiz again
+while restart:
+    # Set score to 0
+    score = 0
+
+    # Loop through each question and ask the player
+    for i in range(len(questions)):
+        # Ask the question
+        print(questions[i])
+        # Get the player's answer
+        player_answer = input()
+
+        # Check if the player's answer matches the correct answer
+        if player_answer.lower() == answers[i].lower():
+            print("Correct!\n")
+            score += 1
+        else:
+            print("Incorrect.\n")
+
+    # Print the player's score and thanks for playing message
+    print("Well done, {}! You scored {}/{}.".format(player_name, score, len(questions)))
+    print("Thanks for playing the Rangiora High School Quiz!\n")
+
+    # Ask the player if they want to restart the quiz again
+    restart = ask_restart()
